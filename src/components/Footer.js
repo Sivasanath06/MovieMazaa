b4d5b27728abd8f9cc33a325ed6481ca1d1c80c3
Sivasanath06 <msivasanathkumar@gmail.com>
2025-01-17 19:12:35 +0530

    Footer updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,56 +1,69 @@
 import { Link } from "react-router-dom";
+
 export const Footer = () => {
+  const currentYear = new Date().getFullYear(); // Get current year
+
   return (
-    <footer className="p-4 bg-white shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
-      <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
-        © 2030{" "}
-        <Link to="/" className="hover:underline">
-          MovieMazaa
-        </Link>
-        . All Rights Reserved.
-      </span>
-      <ul className="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
-        <li>
-          <a
-            href="https://www.instagram.com/sivasanath_1503/"
-            target="_blank"
-            rel="noreferrer"
-            className="mr-4 hover:underline md:mr-6 "
-          >
-            Instagram
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://www.linkedin.com/in/sivasanath-kumar-medavarapu-442157229/"
-            target="_blank"
-            rel="noreferrer"
-            className="mr-4 hover:underline md:mr-6"
-          >
-            LinkedIn
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://twitter.com"
-            target="_blank"
-            rel="noreferrer"
-            className="mr-4 hover:underline md:mr-6"
-          >
-            Twitter
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://github.com/Sivasanath06"
-            target="_blank"
-            rel="noreferrer"
-            className="hover:underline"
-          >
-            Github
-          </a>
-        </li>
-      </ul>
+    <footer className="bg-gray-900 text-white p-6 mt-8">
+      <div className="container mx-auto flex flex-col items-center justify-between md:flex-row md:items-center md:justify-between">
+        {/* MovieMazaa Copyright */}
+        <span className="text-sm text-gray-400 sm:text-center">
+          © {currentYear}{" "}
+          <Link to="/" className="font-semibold hover:text-blue-500">
+            MovieMazaa
+          </Link>
+          . All Rights Reserved.
+        </span>
+
+        {/* Social Media Links */}
+        <ul className="flex flex-wrap items-center mt-3 space-x-6 sm:mt-0">
+          <li>
+            <a
+              href="https://www.instagram.com/sivasanath_1503/"
+              target="_blank"
+              rel="noreferrer"
+              className="text-gray-400 hover:text-blue-500 transition"
+            >
+              Instagram
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.linkedin.com/in/sivasanath-kumar-medavarapu-442157229/"
+              target="_blank"
+              rel="noreferrer"
+              className="text-gray-400 hover:text-blue-500 transition"
+            >
+              LinkedIn
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://twitter.com"
+              target="_blank"
+              rel="noreferrer"
+              className="text-gray-400 hover:text-blue-500 transition"
+            >
+              Twitter
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://github.com/Sivasanath06"
+              target="_blank"
+              rel="noreferrer"
+              className="text-gray-400 hover:text-blue-500 transition"
+            >
+              Github
+            </a>
+          </li>
+        </ul>
+      </div>
+
+      {/* "Made by Sivasanath Kumar M" Text */}
+      <div className="mt-6 text-center text-sm text-gray-400">
+        Made with ❤️ by <strong>Sivasanath Kumar M</strong>
+      </div>
     </footer>
   );
 };
